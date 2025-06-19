9eb7cd0ad48be707a5311724531d7c0baf7c5912
CROMABLUE <jsmoura.dev@gmail.com>
2025-06-19 13:00:12 -0400

    :sparkles: Feat: now alert when the player miss the number

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,7 +1,10 @@
 alert('Boas vindas ao jogo do número secreto');
 let numeroSecreto = 29;
+console.log('O número secreto é: ' + numeroSecreto);
 let chute =prompt('Escolha um numero entre 1 e 30')
 
 if (numeroSecreto == chute) {
     console.log('Isso ai! Você descobriu o numero secreto! (29)')
+}   else{
+    alert('Você errou :(')
 }
