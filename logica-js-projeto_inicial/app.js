e55af478a11493807ef7f9b4aaf434db440b6409
CROMABLUE <jsmoura.dev@gmail.com>
2025-06-19 12:55:16 -0400

    :sparkles: Feat: prompt now save the number

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,3 +1,7 @@
 alert('Boas vindas ao jogo do número secreto');
 let numeroSecreto = 29;
-prompt('Escolha um numero entre 1 e 30')
+let chute =prompt('Escolha um numero entre 1 e 30')
+
+if (numeroSecreto == chute) {
+    console.log('Isso ai! Você descobriu o numero secreto! (29)')
+}
