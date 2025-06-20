8ecb9ebc24ae984b98e9e845464d5d62a3be345d
CROMABLUE <jsmoura.dev@gmail.com>
2025-06-20 14:12:42 -0400

    :sparkles: Feat: Now there is a while loop, to the game only end when the player win

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,15 +1,20 @@
 alert('Boas vindas ao jogo do número secreto');
 let numeroSecreto = 29;
 console.log('O número secreto é: ' + numeroSecreto);
-let chute =prompt('Escolha um numero entre 1 e 30')
+let chute;
 
 
-if (numeroSecreto == chute) {
-    alert(`Isso ai! Você descobriu o numero secreto! ${numeroSecreto}`)
-}   else{
-    if (chute > numeroSecreto) {
-        alert(`O numero secreto é maior que ${chute}`)
-    } else {
-        alert(`O numero secreto é menor que ${chute}`)
+while (chute != numeroSecreto) {
+    chute =prompt('Escolha um numero entre 1 e 30')
+    if (numeroSecreto == chute) {
+        alert(`Isso ai! Você descobriu o numero secreto! ${numeroSecreto}`)
+    }   else{
+        if (chute < numeroSecreto) {
+            alert(`O numero secreto é maior que ${chute}`)
+        } else {
+            alert(`O numero secreto é menor que ${chute}`)
+        }
     }
+
+
 }
