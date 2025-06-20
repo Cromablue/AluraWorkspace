8173e4383a9414ffdcbb2cd3dced1d9912ab4d57
CROMABLUE <jsmoura.dev@gmail.com>
2025-06-20 14:17:37 -0400

    :sparkles: Feat: created an attempt counter

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -2,18 +2,19 @@ alert('Boas vindas ao jogo do número secreto');
 let numeroSecreto = 29;
 console.log('O número secreto é: ' + numeroSecreto);
 let chute;
-
+let tentativas = 1;
 
 while (chute != numeroSecreto) {
-    chute =prompt('Escolha um numero entre 1 e 30')
+    chute =prompt('Escolha um numero entre 1 e 30');
     if (numeroSecreto == chute) {
-        alert(`Isso ai! Você descobriu o numero secreto! ${numeroSecreto}`)
+        alert(`Isso ai! Você descobriu o numero secreto! ${numeroSecreto} com ${tentativas} tentativas.`);
     }   else{
         if (chute < numeroSecreto) {
-            alert(`O numero secreto é maior que ${chute}`)
+            alert(`O numero secreto é maior que ${chute}`);
         } else {
-            alert(`O numero secreto é menor que ${chute}`)
+            alert(`O numero secreto é menor que ${chute}`);
         }
+        tentativas++;
     }
 
 
